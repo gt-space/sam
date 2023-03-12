529a26614b2c1852e2106ee8c8460c5b50ec8d21
Patrick Sliwinski <patrick.sliwinski@gatech.edu>
2023-03-12 13:29:31 -0400

    Device discovery

--- src/data/data_loop.rs
+++ src/data/data_loop.rs
@@ -1,40 +1,38 @@
 use std::thread::sleep;
 use std::time::{Duration, Instant, UNIX_EPOCH};
 
+use fs_protobuf_rust::compiled::google::protobuf::Timestamp;
 use fs_protobuf_rust::compiled::mcfs::data;
 use fs_protobuf_rust::compiled::mcfs::device;
-use fs_protobuf_rust::compiled::google::protobuf::Timestamp;
-use quick_protobuf::{serialize_into_vec, deserialize_from_slice};
-use std::borrow::Cow;
+use quick_protobuf::{deserialize_from_slice, serialize_into_vec};
 use rand::{distributions::Uniform, Rng};
+use std::borrow::Cow;
 
 use std::net::UdpSocket;
 
-pub fn begin(frequency: u64) {
+pub fn begin(socket: UdpSocket, frequency: u64) {
     let interval = Duration::from_micros(1000000 / frequency);
     let mut next_time = Instant::now() + interval;
-    
-    loop {
 
+    loop {
         let data_serialized = data_message_formation();
 
-        let socket = UdpSocket::bind("0.0.0.0:24013").expect("Error");
-        socket.send_to(&data_serialized, "192.168.6.1:7201").expect("couldn't send data");
+        socket
+            .send_to(&data_serialized, "192.168.6.1:24013")
+            .expect("couldn't send data");
 
         sleep(next_time - Instant::now());
         next_time += interval;
-
     }
 }
 
-
 fn data_message_formation() -> Vec<u8> {
     let mut rng = rand::thread_rng();
-    let range = Uniform::new(0, 20000000);
+    let range = Uniform::new(0, 20000);
     let mut node_data: Vec<data::NodeData> = Vec::new();
-    for node_id in 1..40 {
-        let offsets: Vec<u32> = (0..10).map(|_| rng.sample(&range)).collect();
-        let data: Vec<u32> = (0..10).map(|_| rng.sample(&range)).collect();
+    for node_id in 1..2 {
+        let offsets: Vec<u32> = (0..5).map(|_| rng.sample(&range)).collect();
+        let data: Vec<f32> = (0..5).map(|_| rng.sample(&range) as f32 * 1.2).collect();
 
         node_data.push(generate_node_data(offsets, data, node_id));
     }
@@ -47,7 +45,7 @@ fn data_message_formation() -> Vec<u8> {
     data_serialized
 }
 
-fn generate_node_data(offsets: Vec<u32>, data: Vec<u32>, node_id: u32) -> data::NodeData<'static> {
+fn generate_node_data(offsets: Vec<u32>, data: Vec<f32>, node_id: u32) -> data::NodeData<'static> {
     let node = device::NodeIdentifier {
         board_id: 10,
         channel: device::Channel::GPIO,
@@ -55,10 +53,13 @@ fn generate_node_data(offsets: Vec<u32>, data: Vec<u32>, node_id: u32) -> data::
     };
 
     let node_data = data::NodeData {
-        timestamp: Some(Timestamp { seconds: 9, nanos: 100 }),
+        timestamp: Some(Timestamp {
+            seconds: 9,
+            nanos: 100,
+        }),
         node: Some(node),
         micros_offsets: offsets,
-        data_points: data::mod_NodeData::OneOfdata_points::u32_array(data::U32Array { data }),
+        data_points: data::mod_NodeData::OneOfdata_points::f32_array(data::F32Array {data: Cow::from(data)})
     };
 
     return node_data;
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use data::data_loop;
 use command::command_loop;
+use data::data_loop;
+use std::net::UdpSocket;
 use std::thread;
 
-pub mod data;
 pub mod command;
+pub mod data;
 
 fn main() {
-    let command_loop = thread::spawn(|| data_loop::begin(1000));
-    let data_loop = thread::spawn(|| command_loop::begin());
-
+    let socket = UdpSocket::bind("0.0.0.0:24013").expect("Cannot bind to socket");
+    let command_socket = socket.try_clone().expect("Could not clone socket");
+    let data_socket = socket.try_clone().expect("Could not clone socket");
+    let data_loop = thread::spawn(|| data_loop::begin(data_socket, 1));
+    let command_loop = thread::spawn(|| command_loop::begin(command_socket));
 
     command_loop.join().unwrap();
     data_loop.join().unwrap();
