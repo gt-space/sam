7b1a9808234f6b5572eae5ce4f5f7d374e880ba9
Patrick Sliwinski <patrick.sliwinski@gatech.edu>
2023-04-22 15:26:15 -0400

    Send device discovery messages

--- src/discovery/discovery_loop.rs
+++ src/discovery/discovery_loop.rs
@@ -1,19 +1,19 @@
 use std::net::{Ipv4Addr, UdpSocket};
+use std::time::Duration;
 
 use fs_protobuf_rust::compiled::mcfs::core;
 use fs_protobuf_rust::compiled::mcfs::device;
 use fs_protobuf_rust::compiled::mcfs::status;
-use fs_protobuf_rust::compiled::mcfs::command;
-use quick_protobuf::deserialize_from_slice;
 use quick_protobuf::{serialize_into_vec};
 
-pub fn begin() {
+pub fn begin(broadcast_period: Duration) {
     let mcast_group: Ipv4Addr = "224.0.0.3".parse().unwrap();
     let port: u16 = 6000;
     let any = "0.0.0.0".parse().unwrap();
 
     let socket = UdpSocket::bind((any, port)).expect("Could not bind client socket");
     socket.set_multicast_loop_v4(false).expect("set_multicast_loop_v4 call failed");
+    socket.set_read_timeout(Some(Duration::new(10, 0))).expect("set_read_timeout call failed");
     socket
         .join_multicast_v4(&mcast_group, &any)
         .expect("Could not join multicast group");
@@ -31,30 +31,28 @@ pub fn begin() {
     };
 
     let response_serialized = serialize_into_vec(&response).expect("Could not serialize discovery response");
+    let mut last_broadcasted = std::time::Instant::now();
 
-
-
-    let mut buffer = [0u8; 1600];
 
     loop {
-        let result = socket.recv_from(&mut buffer);
-        match result {
-            Ok((_size, src)) => {
-                // TODO: log discovery message
-                if let Ok(core::Message {
-                    content: core::mod_Message::OneOfcontent::command(
-                        command::Command {
-                            command: command::mod_Command::OneOfcommand::device_discovery(..)
-                        }
-                    ),..}
-                ) = deserialize_from_slice(&buffer) {
-                    println!("Received discovery message from {}", src);
-                    let _result = socket.send_to(&response_serialized, &(mcast_group, port));
-                } 
-            }
-            Err(_e) => {
-                // TODO: log error
-            }
+        read_device_info(&socket);
+        let time_since_last_broadcast = last_broadcasted.elapsed();
+        if time_since_last_broadcast > broadcast_period {
+            let _send_result = socket.send_to(&response_serialized, (mcast_group, port));
+            last_broadcasted = std::time::Instant::now();
+            println!("Sent device info");
         }
     }
+}
+
+/*
+ * Listen on the socket for a device info message
+ * The SAM boards currently do not store any information on other devices
+ * so we can just ignore the message
+ * 
+ * The recv-from call will timeout and return to the discovery loop
+ */
+pub fn read_device_info(socket: &UdpSocket) {
+    let mut buffer = [0u8; 1600];
+    let _result = socket.recv_from(&mut buffer);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use command::command_loop;
 use data::data_loop;
 use discovery::discovery_loop;
-use std::thread;
+use std::{thread, time::Duration};
 
 pub mod command;
 pub mod data;
 pub mod discovery;
 
 fn main() {
-    let data_loop = thread::spawn(|| data_loop::begin(1));
+    let data_loop = thread::spawn(|| data_loop::begin(500));
     let command_loop = thread::spawn(|| command_loop::begin());
-    let discovery_loop = thread::spawn(|| discovery_loop::begin());
+    let discovery_loop = thread::spawn(|| discovery_loop::begin(Duration::from_secs(5)));
 
     command_loop.join().unwrap();
     data_loop.join().unwrap();
